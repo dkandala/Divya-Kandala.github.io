9425b34c05522673a2ab3558969e6365ccfda46a
Divya Kandala <dkandala@gmail.com>
2025-10-23 15:37:49 -0600

    Update ica10.js

--- week-10/ica10.js
+++ week-10/ica10.js
@@ -1,50 +1,42 @@
-let triviaBtn = document.querySelector("#js-new-quote").
-addEventListener("click",newTrivia);
+let triviaBtn = document.querySelector("#js-new-quote").addEventListener('click', newTrivia);
 
-let answerBtn = document.querySelector('#js-tweet').addEventListener('click',newAnswer);
+let answerBtn = document.querySelector('#js-tweet').addEventListener('click', newAnswer);
 
 let current = {
-    question:"",
-    answer:"",
+    question: "",
+    answer: "",
 }
 
-const endpoint= "https://trivia.cyberwisp.com/getrandomchristmasquestion";
+const endpoint = "https://trivia.cyberwisp.com/getrandomchristmasquestion";
 
 async function newTrivia() {
-    //console.log("Success");
+    // console.log("Success");
     try {
-    const response = await fetch(endpoint);
-    if (!response.ok){
-        throw Error(response.statusText)
+        const response = await fetch(endpoint);
+        if (!response.ok) {
+            throw Error(response.statusText)
         }
         const json = await response.json();
-        console.log(json);
         displayTrivia(json["question"]);
-        current.question = json['question'];
-        current.answer= json['answer'];
-        console.log(current.question);
-        console.log(current.answer);
-
-    }
-    catch(err){
+        current.question = json["question"];
+        current.answer = json["answer"];
+     } catch (err) {
         console.log(err)
-        alert('failed to get new trivia')
-    }
+        alert('Failed to get new trivia');
+     }
+}
 
-}   
-
-function displayTrivia(question){
-    const questionText = document.querySelector("#js-quote-text");
-    const answerText = document.querySelector('#js-answer-text');
-
+function displayTrivia(question) {
+    const questionText = document.querySelector('#js-quote-text');
+    const answerText = document.querySelector("#js-answer-text");
     questionText.textContent = question;
     answerText.textContent = "";
-
 }
-function newAnswer(){
-    console.log('Success == answer!');
 
-    const answerText = document.querySelector('#js-answer-text');
-    answerText.textContent = current.answer
+function newAnswer() {
+    //console.log("Success == answer!");
+    const answerText = document.querySelector("#js-answer-text");
+    answerText.textContent = current.answer;
 }
+
 newTrivia();
